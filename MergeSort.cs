b8e08d49c44a9059c1cbe2eb6e8c933b5e6a5fdc
yashr <yashr.shah@outlook.com>
2022-05-29 12:27:58 +0530

    added merge sort

--- MergeSort.cs
+++ MergeSort.cs
@@ -8,5 +8,81 @@ namespace SortingAlgo
 {
     internal class MergeSort
     {
+        public void Sort(int[] arr)
+        {
+            mergeSort(arr, 0, arr.Length - 1);
+            Console.WriteLine("Sorted input array: [{0}]", string.Join(", ", arr));
+        }
+        private void mergeSort(int[] arr, int startIndex, int endIndex)
+        {
+            if (startIndex < endIndex)
+            {
+                var midIndex = (startIndex + endIndex) / 2;
+                mergeSort(arr, startIndex, midIndex);
+                mergeSort(arr, midIndex + 1, endIndex);
+                Combine(arr, startIndex, midIndex, endIndex);
+            }      
+        }
+        void Combine(int[] arr, int startIndex, int midIndex, int endIndex)
+        {
+            // Find sizes of two
+            // subarrays to be merged
+            int n1 = midIndex - startIndex + 1;
+            int n2 = endIndex - midIndex;
+
+            // Create temp arrays
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            // Copy data to temp arrays
+            for (i = 0; i < n1; ++i)
+                L[i] = arr[startIndex + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = arr[midIndex + 1 + j];
+
+            // Merge the temp arrays
+
+            // Initial indexes of first
+            // and second subarrays
+            i = 0;
+            j = 0;
+
+            // Initial index of merged
+            // subarray array
+            int k = startIndex;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            // Copy remaining elements
+            // of L[] if any
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            // Copy remaining elements
+            // of R[] if any
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -15,10 +15,12 @@ namespace SortingAlgo
                 arr[i] = rand.Next(101);
             }
             Console.WriteLine("Random input array: [{0}]", string.Join(", ", arr));
-            Console.WriteLine("Press 1 for Simple sort." +
+            Console.WriteLine("" +
+                  "Press 1 for Simple sort." +
                 "\nPress 2 for Selection sort" +
                 "\nPress 3 for Bubble sort"+
-                 "\nPress 4 for Insertion sort"
+                "\nPress 4 for Insertion sort"+
+                "\nPress 5 for Merge sort"
                 );
             int sortAlgo = Convert.ToInt32(Console.ReadLine());
             switch (sortAlgo)
@@ -40,7 +42,8 @@ namespace SortingAlgo
                     insert.Sort(arr);
                     break;
                 case 5:
-                    Console.WriteLine("Friday");
+                    var merge = new MergeSort();
+                    merge.Sort(arr);
                     break;
                 case 6:
                     Console.WriteLine("Saturday");
